6a3f6b27a4fdbc78f6b80e59be4f95ef4d11659c
shuyun <shuyungogogo@hotmail.com>
2018-11-09 17:54:34 +0800

    clear some files and update gradle to 4.6

--- app/src/main/java/shuyun/opencv4android/MainActivity.java
+++ app/src/main/java/shuyun/opencv4android/MainActivity.java
@@ -1,109 +1,17 @@
 package shuyun.opencv4android;
 
-import android.content.Context;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 
-import org.opencv.android.CameraBridgeViewBase;
-import org.opencv.android.JavaCameraView;
-import org.opencv.android.OpenCVLoader;
-import org.opencv.core.Core;
-import org.opencv.core.Mat;
-import org.opencv.core.MatOfRect;
-import org.opencv.core.Rect;
-import org.opencv.core.Scalar;
-import org.opencv.core.Size;
-import org.opencv.imgproc.Imgproc;
-import org.opencv.objdetect.CascadeClassifier;
-
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.InputStream;
-
-public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
-
-    private JavaCameraView cameraView;
-    private CascadeClassifier cascadeClassifier;
-    private Mat mRgba;
-    private Mat mGray;
-    private int absoluteFaceSize = 0;
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        cameraView = findViewById(R.id.cameraView);
-        cameraView.setCameraIndex(0);
-        cameraView.enableFpsMeter();
 
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        if (!OpenCVLoader.initDebug()) {
-            Log.e("test", "init error");
-        }
-        initOpenCV();
-    }
 
-    private void initOpenCV(){
-        try {
-            InputStream is = getResources().openRawResource(R.raw.lbpcascade_frontalface_improved); //OpenCV的人脸模型文件： lbpcascade_frontalface_improved
-            File cascadeDir = getDir("cascade", Context.MODE_PRIVATE);
-            File mCascadeFile = new File(cascadeDir, "lbpcascade_frontalface_improved.xml");
-            FileOutputStream os = new FileOutputStream(mCascadeFile);
-            byte[] buffer = new byte[4 * 1024];
-            int bytesRead;
-            while ((bytesRead = is.read(buffer)) != -1) {
-                os.write(buffer, 0, bytesRead);
-            }
-            is.close();
-            os.close();
-            cascadeClassifier = new CascadeClassifier(mCascadeFile.getAbsolutePath());
-        } catch (Exception e) {
-            Log.e("test", "Error loading cascade", e);
-        }
-        cameraView.enableView();
-    }
-
-    @Override
-    public void onCameraViewStarted(int width, int height) {
-        mRgba = new Mat();
-        mGray = new Mat();
-    }
-
-    @Override
-    public void onCameraViewStopped() {
-        mRgba.release();
-        mGray.release();
-    }
-
-    @Override
-    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
-        Log.e("test", "onCameraFrame");
-        mRgba = inputFrame.rgba();
-        mGray = inputFrame.gray();
-
-        if (absoluteFaceSize == 0) {
-            int height = mGray.rows();
-            if (Math.round(height * 0.2f) > 0) {
-                absoluteFaceSize = Math.round(height * 0.2f);
-            }
-        }
-
-        MatOfRect faces = new MatOfRect();
-        if (cascadeClassifier != null) {
-            cascadeClassifier.detectMultiScale(mGray, faces, 1.1, 2, 2, new Size(absoluteFaceSize, absoluteFaceSize), new Size());
-        }
-        Rect[] facesArray = faces.toArray();
-        if (facesArray.length > 0){
-            for (int i = 0; i < facesArray.length; i++) {
-                Imgproc.rectangle(mRgba, facesArray[i].tl(), facesArray[i].br(), new Scalar(0, 255, 0, 255), 3);
-            }
-        }
-        return mRgba;
-    }
 }
