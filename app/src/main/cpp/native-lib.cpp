ad271b03db4a5245df8d86fbf0229c9e34f7b86b
shuyun <shuyungogogo@hotmail.com>
2018-11-10 12:32:26 +0800

    update the CMake file, and write a sample code in jni

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,5 +1,7 @@
 #include <jni.h>
 #include <string>
+#include <android/bitmap.h>
+#include <opencv2/opencv.hpp>
 
 using namespace cv;
 
@@ -7,10 +9,34 @@ extern "C"
 JNIEXPORT jstring JNICALL
 Java_shuyun_opencv4android_MainActivity_stringFromJNI(
         JNIEnv *env,
-        jobject /* this */) {
+        jobject /* this */,
+        jobject bitmap) {
     std::string hello = "Hello from C++";
-    Mat img;
 
+    AndroidBitmapInfo info;
+    void *pixels;
+
+//    int res = AndroidBitmap_getInfo(env, bitmap, &info);
+
+    CV_Assert(AndroidBitmap_getInfo(env, bitmap, &info) >= 0);
+    CV_Assert(info.format == ANDROID_BITMAP_FORMAT_RGBA_8888 ||
+              info.format == ANDROID_BITMAP_FORMAT_RGB_565);
+    CV_Assert(AndroidBitmap_lockPixels(env, bitmap, &pixels) >= 0);
+    CV_Assert(pixels);
+    if (info.format == ANDROID_BITMAP_FORMAT_RGBA_8888) {
+        Mat temp(info.height, info.width, CV_8UC4, pixels);
+        Mat gray;
+        cvtColor(temp, gray, COLOR_RGBA2GRAY);
+        Canny(gray, gray, 125, 225);
+        cvtColor(gray, temp, COLOR_GRAY2RGBA);
+    } else {
+        Mat temp(info.height, info.width, CV_8UC2, pixels);
+        Mat gray;
+        cvtColor(temp, gray, COLOR_RGB2GRAY);
+        Canny(gray, gray, 125, 225);
+        cvtColor(gray, temp, COLOR_GRAY2RGB);
+    }
+    AndroidBitmap_unlockPixels(env, bitmap);
 
     return env->NewStringUTF(hello.c_str());
 }
--- app/src/main/java/shuyun/opencv4android/MainActivity.java
+++ app/src/main/java/shuyun/opencv4android/MainActivity.java
@@ -1,17 +1,34 @@
 package shuyun.opencv4android;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    static {
+        System.loadLibrary("native-lib");
+    }
+
+    private ImageView ivTips;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ivTips = findViewById(R.id.ivTips);
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher);
+        stringFromJNI(bitmap);
+        ivTips.setImageBitmap(bitmap);
+
     }
+
+    public native String stringFromJNI(Bitmap bitmap);
 
 
 }
