ca92b49136ac51fead7ab935a7fe9d9f5c8f0739
shuyun <shuyungogogo@hotmail.com>
2018-11-10 17:14:02 +0800

    Add Android native log Add Java layer codes

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -2,12 +2,17 @@
 #include <string>
 #include <android/bitmap.h>
 #include <opencv2/opencv.hpp>
+#include <android/log.h>
+
+#define LOG_TAG "native-lib_JNI"
+#define LOGD(...) ((void)__android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, __VA_ARGS__))
+#define LOGE(...) ((void)__android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__))
 
 using namespace cv;
 
 extern "C"
 JNIEXPORT jstring JNICALL
-Java_shuyun_opencv4android_MainActivity_stringFromJNI(
+Java_shuyun_opencv4android_MainActivity_processImage(
         JNIEnv *env,
         jobject /* this */,
         jobject bitmap) {
@@ -15,7 +20,8 @@ Java_shuyun_opencv4android_MainActivity_stringFromJNI(
 
     AndroidBitmapInfo info;
     void *pixels;
-
+    LOGD("start process bitmap");
+    LOGE("start process bitmap(ERROR TEST)");
 //    int res = AndroidBitmap_getInfo(env, bitmap, &info);
 
     CV_Assert(AndroidBitmap_getInfo(env, bitmap, &info) >= 0);
--- app/src/main/java/shuyun/opencv4android/MainActivity.java
+++ app/src/main/java/shuyun/opencv4android/MainActivity.java
@@ -5,8 +5,14 @@ import android.graphics.BitmapFactory;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import org.opencv.android.OpenCVLoader;
+import org.opencv.android.Utils;
+import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,12 +29,36 @@ public class MainActivity extends AppCompatActivity {
 
         ivTips = findViewById(R.id.ivTips);
         Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher);
-        stringFromJNI(bitmap);
+//        stringFromJNI(bitmap);
+//        ivTips.setImageBitmap(bitmap);
+        //Before using OpenCV java code, must init OpenCV like this method
+        //then invoke OpenCV relative methods
+        initLoadOpenCV();
+        Mat src = new Mat();
+        Mat dst = new Mat();
+        Utils.bitmapToMat(bitmap, src);
+        Imgproc.cvtColor(src, dst, Imgproc.COLOR_BGRA2GRAY);
+        Utils.matToBitmap(dst, bitmap);
         ivTips.setImageBitmap(bitmap);
-
+        src.release();
+        dst.release();
     }
 
-    public native String stringFromJNI(Bitmap bitmap);
+    private void initLoadOpenCV(){
+        boolean success = OpenCVLoader.initDebug();
+        if (success) {
+            Log.e("test", "init opencv success");
+        } else {
+            Log.e("test", "init opencv failed");
+        }
+    }
+
+    /**
+     * invoke native method to process bitmap
+     * @param bitmap
+     * @return
+     */
+    public native String processImage(Bitmap bitmap);
 
 
 }
