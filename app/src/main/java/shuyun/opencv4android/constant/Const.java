ad0a7f724a0533d70de24c98d3538d25d1c6b381
shuyun <shuyungogogo@hotmail.com>
2019-03-12 00:39:52 +0800

    in

---
+++ app/src/main/java/shuyun/opencv4android/constant/Const.java
@@ -0,0 +1,24 @@
+package shuyun.opencv4android.constant;
+
+public class Const {
+
+    public static float VERTEX[] = {
+            1.0f, -1.0f,
+            -1.0f, -1.0f,
+            1.0f, 1.0f,
+            -1.0f, 1.0f
+    };
+    public static float COORD1[] = {
+            1.0f, 0.0f,
+            0.0f, 0.0f,
+            1.0f, 1.0f,
+            0.0f, 1.0f
+    };
+    public static float COORD2[] = {
+            0.0f, 1.0f,
+            1.0f, 1.0f,
+            0.0f, 0.0f,
+            1.0f, 0.0f
+    };
+
+}
